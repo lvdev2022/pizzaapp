fff0ac2c83b494b81b66250608763c72ee4e4122
Lam <lvdev2022@gmail.com>
2022-09-12 21:08:02 -0700

    added image, state and slider

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,4 @@
-
+import Image from "next/image";
 import styles from "../styles/Navbar.module.css";
 
 
@@ -6,7 +6,32 @@ const Navbar = () => {
 
   return (
     <div className={styles.container}>
-      NavBar
+      <div className={styles.item}>
+       <div className={styles.callButton}>
+          <Image src="/img/telephone.png" alt="" width="32" height="32"/>
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW !</div>
+          <div className={styles.text}>(858) 323 - 9999</div>
+        </div>
+
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Home</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <Image src="/img/logo.png" alt="" width="160px" height="69px"/>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+        <Image src="/img/cart.png" alt="" width="30px" height="30px"/>
+        <div className={styles.counter}>2</div>
+        </div>
+      </div>
     </div>
   );
 };
