607f4cfad0e7dbdcfa2ed3fcdad40e4e9df68f4e
Lam <lvdev2022@gmail.com>
2022-09-12 19:32:13 -0700

    added Layout Navbar Footer

---
+++ components/Navbar.jsx
@@ -0,0 +1,14 @@
+
+import styles from "../styles/Navbar.module.css";
+
+
+const Navbar = () => {
+
+  return (
+    <div className={styles.container}>
+      NavBar
+    </div>
+  );
+};
+
+export default Navbar;
